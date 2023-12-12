5309ebdb4063587cdc5e19b475a609cc053f4b66
ThiagoBechara <95245669+ThiagoBechara@users.noreply.github.com>
2023-12-12 16:11:27 -0300

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+from_dir='c:/Users/Cliente/Downloads'
+to_dir='C:\Users\Cliente\Desktop\Arquivos_Documentos'
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
